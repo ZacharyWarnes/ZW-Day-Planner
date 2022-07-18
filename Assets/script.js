7945d088a71e7d029a4fba501ad87ef136d74dc9
Zachary Warnes <zacharywarnes@gmail.com>
2022-07-17 23:18:11 -0700

    added js code for local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,6 @@
 
 //I am declaring the current hour for comparative use later 
 var currentHour = moment().hour();
-  console.log(currentHour);
 
 //Here is the code for displaying the current date at the top of the page
 var today = moment().format("dddd MMMM Do");
@@ -11,15 +10,19 @@ $("#currentDay").text(today);
 var timeEntriesEl = $(".description");
 
 //Declaring a save button variable
-var button = $(".saveBtn");
 
 for (var i = 9; i <= 17; i++) {
+
+ 
+  var data = JSON.parse(localStorage.getItem(i)) || "";
+ 
+  
+  console.log(data); 
   
 // Set a variable to create 9AM-5PM daily schedule converted from a 24-hour clock
 var realTime;
 
   if (i < 12 ){
-    console.log("AM")
     realTime = i + "AM";
   }
 
@@ -28,7 +31,6 @@ var realTime;
   }
   
   else {
-    console.log ("PM")
     realTime = (i -12) + "PM";
   }
   // Compare i to current hour to determine of this hour is in the past, present, or future
@@ -52,8 +54,8 @@ var colorClass;
     <div class = "hour col">
       ${realTime}
     </div>
-      <textarea class= "description col-10 ${colorClass}"></textarea>
-      <button class = "saveBtn col" data-hour="${i}">Save</button>
+      <textarea class= "description col-10 ${colorClass}">${data}</textarea>
+      <button class = "saveBtn col" data-hour="${i}">💾</button>
     
 </div>
 `;
@@ -64,21 +66,17 @@ $("#timeEntries").append(template);
 
 }
 
+var button = $(".saveBtn");
+console.log(button);
 //This Code Saves the Time Entry blocks to local storage
 
 button.on("click", function(event) {
- var savedTimeEntry = timeEntriesEl.textContent;
- console.log(savedTimeEntry);
- localStorage.setItem('key', JSON.stringify(savedTimeEntry));
- timeEntriesEl();
- realTime();
+var hourKey = event.target.dataset.hour;
+var textEntry = event.target.previousSibling.previousElementSibling.value;
+console.log(hourKey,textEntry);
+localStorage.setItem(hourKey,JSON.stringify(textEntry));
 });
 
-function getValue() {
-  return localStorage.getItem('timeEntriesEl');
-  return localStorage.getItem('realTime');
-}
-console.log(getValue());
 
 //localStorage.getItem
 
